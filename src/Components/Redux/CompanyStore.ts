1cba9d0ac8e95a420becaf2f24d6254e0389b21e
Tomer2H <tomer2b0525@gmail.com>
2024-02-17 01:15:18 +0200

    added the start of companyCoupons redux still missing reducers.

--- src/Components/Redux/CompanyStore.ts
+++ src/Components/Redux/CompanyStore.ts
@@ -1,2 +1,33 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import Coupon from "../Models/Coupon";
 
-export {};
+export interface CompanyCouponsState{
+    coupon: Coupon[];
+}
+
+
+const initState: CompanyCouponsState = {
+    coupon: []
+  
+}
+
+export const CompanyCouponsSlice = createSlice({
+    name: "CompanyCoupons",
+    initialState: initState,
+    reducers: {
+    
+
+
+}})
+
+
+export const { } = CompanyCouponsSlice.actions;
+export default CompanyCouponsSlice.reducer;
+
+
+export const companyCouponsStore = configureStore({
+    reducer: CompanyCouponsSlice.reducer
+})
+
+export type RootState = ReturnType<typeof companyCouponsStore.getState>
+export type AppDispach = typeof companyCouponsStore.dispatch
