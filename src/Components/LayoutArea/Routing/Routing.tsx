922a8bc977296257e52b443c012594c6467377e6
Tomer2H <tomer2b0525@gmail.com>
2024-02-17 23:34:44 +0200

    Changes to Routing added default routing to home Page and added additional pages

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes , Navigate} from "react-router-dom";
 import "./Routing.css";
 import Login from "../../Pages/LoginPage/Login";
 import MyDetails from "../../Pages/MyDetails/MyDetails";
 import Home from "../../Pages/Home/Home";
 import AddCoupon from "../../Pages/CompanyArea/AddCoupon/AddCoupon";
+import CouponDetails from "../../Pages/Home/CouponDetails/CouponDetails";
+import NotFoundPage from "../../Pages/NotFoundPage/NotFoundPage";
 
 function Routing(): JSX.Element {
     return (
         <div className="Routing">
 			<Routes>
-                
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/MyDetails" element={<MyDetails/>} />
-                <Route path="/addCoupon" element={<AddCoupon/>} />
-                <Route path="" element={<Home />}/>
+                <Route path="/home" element={<Home/>} />
+                <Route path="" element={<Navigate to={"/home"}/>} />
+                <Route path="/login" element={<Login />}/>
+                <Route path="/MyDetails" element={<MyDetails />} />
+                <Route path="/addCoupon" element={<AddCoupon />} />
+                <Route path="/coupon/:couponId" element={<CouponDetails />} />
+                <Route path="*" element={<NotFoundPage/>} />
             </Routes>
         </div>
     );
