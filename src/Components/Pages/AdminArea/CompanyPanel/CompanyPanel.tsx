8f99ef37213a7e2c2cda7de27d1cf0a783c1a98a
Tomer2H <tomer2b0525@gmail.com>
2024-02-17 23:45:18 +0200

    folder directory changes

---
+++ src/Components/Pages/AdminArea/CompanyPanel/CompanyPanel.tsx
@@ -0,0 +1,11 @@
+import "./CompanyPanel.css";
+
+function CompanyPanel(): JSX.Element {
+    return (
+        <div className="CompanyPanel">
+			
+        </div>
+    );
+}
+
+export default CompanyPanel;
